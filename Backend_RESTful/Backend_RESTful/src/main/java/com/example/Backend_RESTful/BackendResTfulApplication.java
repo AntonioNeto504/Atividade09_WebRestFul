7970c5f14445843cf51797714da8f45c54bfd825
AntonioNeto504 <antoniocustorioneto@gmail.com>
2024-11-07 13:21:02 -0300

    Atividade 09 - Backend RESTful

---
+++ Backend_RESTful/Backend_RESTful/src/main/java/com/example/Backend_RESTful/BackendResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_RESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendResTfulApplication.class, args);
+	}
+
+}
---
+++ Backend_RESTful/Backend_RESTful/src/test/java/com/example/Backend_RESTful/BackendResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_RESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
